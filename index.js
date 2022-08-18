3daff94d8dd4fc86b31980c43bb6b32dacdfc75e
henry-thiongi <boblilhot@gmail.com>
2022-08-18 04:32:35 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+function divide() {
+    let a=2000;
+    let b=100;
+    return a/b
+  }
+  divide();
+  const square = (parameter1) => parameter1**2;
+square(2); 
+const add = (parameter1, parameter2) => parameter1 + parameter2;
+add(2,3);
